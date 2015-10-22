107f9961001d9921307080d90d2c315bf3be12a8
juyingnan <bunny_gg@hotmail.com>
2015-10-22 12:02:22 +0800

    Create Midterm2.py

---
+++ Midterm2.py
@@ -0,0 +1,24 @@
+#!/bin/python
+
+# Yue Chen
+
+import fileinput
+
+fileName = raw_input("Please input the file name: ")
+
+tags = []
+counts = []
+
+for line in fileinput.input(fileName):
+    words = line.split("\t")
+    if len(words) > 1:
+        if words[1] not in tags:
+            tags.append(words[1])
+            counts.append(0)
+        counts[tags.index(words[1])] += 1
+
+i = 0
+
+while i < len(tags):
+	print tags[i], counts[i]
+	i += 1
