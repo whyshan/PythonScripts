7df04ccd9345f83bf61d573f8aa3d12d072055f8
juyingnan <bunny_gg@hotmail.com>
2015-10-22 12:27:38 +0800

    Update Midterm3.py

--- Midterm3.py
+++ Midterm3.py
@@ -1,20 +1,20 @@
-num = 1
+num = 1 # Error 1, "num = 0" => "num = 1"
 numlist = []
 
 while num != 0:
     num = int(raw_input('next num: '))
     numlist.append(num)
-numlist.pop()
+numlist.pop() # Error 2, pop 0
 numlist.sort()
 
-newnum = int(raw_input('new num: '))
+newnum = int(raw_input('new num: ')) # Error 3, int()
 done = 0
 cnt = 0
-while not done and cnt < len(numlist):
+while not done and cnt < len(numlist): # Error 4: add ":"
     if newnum < numlist[cnt]:
         numlist.insert(cnt, newnum)
         done = 1
-    cnt+=1
+    cnt+=1 # Error 5
 if not done:
     numlist.append(newnum)
 
