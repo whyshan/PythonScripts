52c0647360ae4ec85ae5b491b4ad11c5fc5ba503
Yingnan Ju <bunny_gg@hotmail.com>
2015-11-05 13:44:44 +0800

    update feature extraction demo, fix '@' and '#' , and other punctuation issue

--- assign4ques3.py
+++ assign4ques3.py
@@ -9,6 +9,7 @@ punctuation = string.punctuation
 # Read in by line and process
 
 words = []
+bigrams = []
 last = ""
 
 blonde = open("blonde.txt","r")
@@ -18,9 +19,9 @@ for eachline in blonde:
 			eachline = eachline.replace(char, " " + char + " ")
 			words = eachline.split()
 			if len(words) < 1:
-        		break
-   			if (len(last) > 1):
-        		bigrams.append([last, words[0]])
-    		for i in range(0, len(words)-1):
-        		bigrams.append([words[i], words[i+1]])
-    			  last = words[-1]
+				break
+			if (len(last) > 1):
+				bigrams.append([last, words[0]])
+			for i in range(0, len(words)-1):
+				bigrams.append([words[i], words[i+1]])
+				last = words[-1]
