1bc2befef2b16914015365050470f4a425747cc3
Yingnan Ju <bunny_gg@hotmail.com>
2015-11-05 15:23:00 +0800

    4-4 updated

--- assign4ques3.py
+++ assign4ques3.py
@@ -25,14 +25,14 @@ for eachline in blonde:
     for char in eachline:
         if char in punctuation:
             eachline = eachline.replace(char, " " + char + " ")
-            words = eachline.split()
-            if len(words) < 1:
-                break
-            if (len(last) >= 1):
-                bigrams.append([last, words[0]])
-            for i in range(0, len(words) - 1):
-                bigrams.append([words[i], words[i + 1]])
-                last = words[-1]
+    words = eachline.split()
+    if len(words) < 1:
+        break
+    if (len(last) >= 1):
+        bigrams.append([last, words[0]])
+    for i in range(0, len(words) - 1):
+        bigrams.append([words[i], words[i + 1]])
+        last = words[-1]
 print bigrams
 
 # Statistic
---
+++ assign4ques4.py
@@ -0,0 +1,35 @@
+#!/bin/python
+
+# Yue Chen
+
+# Read in by line and process
+
+newvm = open("newvm.pos", "r")
+results = []
+
+# Timer start -- begin
+from time import clock
+
+start = clock()
+# Timer start -- end
+import re
+
+pattern = re.compile(r'\b(\w+/DT)(\s)(\w+/JJ)+(\s)(\w+/NN|NNS)')
+
+for eachline in newvm:
+    match = pattern.findall(eachline)
+    if match.__len__() > 0:
+        results.append(match)
+
+# print
+for result in results:
+    for match in result:
+        string = ""
+        for word in match:
+            string += word
+        print string
+
+# Timer end -- begin
+finish = clock()
+print "time: ", finish - start, 's'
+# Timer end -- end
