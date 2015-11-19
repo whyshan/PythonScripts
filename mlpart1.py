5907c3b9454a48e5ca76edcda390624f85c76294
Eleanor Chen <chenyueg@users.noreply.github.com>
2015-11-18 21:10:54 -0500

    Create mlpart1.py

---
+++ mlpart1.py
@@ -0,0 +1,34 @@
+IN = open('cd6.pos')
+words = ['NULL', 'NULL', 'NULL']
+line = 'doobly-doo'
+
+for x in range(0,2):
+    line = IN.readline()
+    wp = line.split()
+    words.append(wp[0])
+
+while line:
+    line = IN.readline()
+    wp = line.split()
+    if len(wp):
+        words.pop(0)
+        words.append(wp[0])
+        for x in words:
+            print x + "\t",
+        print ""
+    else:
+        for z in range (0,2):
+            words.pop(0)
+            words.append('NULL')
+            for x in words:
+                print x + "\t",
+            print ""
+        words = ['NULL', 'NULL', 'NULL']
+        for x in range(0,2):
+            line = IN.readline()
+            if len(line) < 1:
+                break
+            wp = line.split()
+            words.append(wp[0])
+
+IN.close()
