c8fbde4ea1de9ff85d12bb1887a710d96eae7043
Yingnan Ju <bunny_gg@hotmail.com>
2016-03-06 12:19:55 +0800

    format adjusted

--- xml_parser_test.py
+++ xml_parser_test.py
@@ -1,13 +1,16 @@
 from xml.dom import minidom
 xmldoc = minidom.parse('test.xml')
+
 instancelist = xmldoc.getElementsByTagName('instance')
 print(len(instancelist))
 for instance in instancelist:
     print(instance.attributes['id'].value)
+
 answerlist = xmldoc.getElementsByTagName('answer')
 print(len(answerlist))
 for answer in answerlist:
     print(answer.attributes['senseid'].value)
+
 contextlist = xmldoc.getElementsByTagName('context')
 print(len(contextlist))
 for context in contextlist:
